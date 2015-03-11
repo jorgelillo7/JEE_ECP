8d0f7c3e8905d6cd50b15180a492f85420bf5823
Jorge Lillo <jorge.lillo9@gmail.com>
2015-03-11 20:53:33 +0100

    Clase Tema implementada

--- src/main/java/models/entities/Tema.java
+++ src/main/java/models/entities/Tema.java
@@ -1,5 +1,50 @@
 package models.entities;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
 public class Tema {
+	public static final String TABLE = "tema";
+
+	public static final String ID = "ID";
+
+	@Id
+	@GeneratedValue
+	private Integer id;
+
+	public static final String PREGUNTA = "PREGUNTA";
+	private String pregunta;
+
+	public Tema() {
+	}
+
+	public Tema(Integer id, String pregunta) {
+		super();
+		this.id = id;
+		this.pregunta = pregunta;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getPregunta() {
+		return pregunta;
+	}
+
+	public void setPregunta(String pregunta) {
+		this.pregunta = pregunta;
+	}
+
+	@Override
+	public String toString() {
+		return "Tema [id=" + id + ", pregunta=" + pregunta + "]";
+	}
 
 }
