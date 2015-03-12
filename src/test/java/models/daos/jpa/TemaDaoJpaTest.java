1ee9ec7cbd487a7fc4b43b5e84bf4a5018913f96
Jorge Lillo <jorge.lillo9@gmail.com>
2015-03-12 21:18:51 +0100

    Fin sesión de trabajo

--- src/test/java/models/daos/jpa/TemaDaoJpaTest.java
+++ src/test/java/models/daos/jpa/TemaDaoJpaTest.java
@@ -13,7 +13,7 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test; 
 
-public class TemaDaoJpaTest {
+public class TemaDaoJpaTest { 
 	public TemaDao temaDaoJpa;
 
 	private Tema tema;
