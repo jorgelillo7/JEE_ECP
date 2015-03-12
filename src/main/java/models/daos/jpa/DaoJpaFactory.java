df85dc5da47a28add6e392c1352d590730003dac
Jorge Lillo <jorge.lillo9@gmail.com>
2015-03-12 20:49:17 +0100

    Dao jpa factory con unidad de persistencia correcta y añadido dropAndCreate tables

--- src/main/java/models/daos/jpa/DaoJpaFactory.java
+++ src/main/java/models/daos/jpa/DaoJpaFactory.java
@@ -4,11 +4,14 @@ import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
 import org.apache.logging.log4j.LogManager;
-
+import java.util.HashMap;
+import java.util.Map;
+ 
+import org.eclipse.persistence.config.PersistenceUnitProperties;
 import models.daos.DaoFactory;
 
 public class DaoJpaFactory extends DaoFactory {
-    private static final String PERSISTENCE_UNIT = "tictactoe";
+    private static final String PERSISTENCE_UNIT = "JEE_ECP";
 
     private static EntityManagerFactory entityManagerFactory = Persistence
             .createEntityManagerFactory(PERSISTENCE_UNIT);
@@ -21,6 +24,12 @@ public class DaoJpaFactory extends DaoFactory {
         return entityManagerFactory;
     }
 
-  
+    public static void dropAndCreateTables() {
+        Map<String, String> properties = new HashMap<>();
+        properties.put(PersistenceUnitProperties.DDL_GENERATION,
+                PersistenceUnitProperties.DROP_AND_CREATE);
+        entityManagerFactory = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT, properties);
+		LogManager.getLogger("Reseto de tablas");
+	}
 
 }
