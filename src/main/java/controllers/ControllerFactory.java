e0f64347ab638394d78241905ce00610b72416bb
Jorge Lillo <jorge.lillo9@gmail.com>
2015-03-19 15:43:47 +0100

    Inicio sesión

--- src/main/java/controllers/ControllerFactory.java
+++ src/main/java/controllers/ControllerFactory.java
@@ -3,4 +3,6 @@ package controllers;
 public abstract class ControllerFactory { 
 	public abstract NuevoTemaController getNuevoTemaControler();
 	public abstract EliminarTemaController getEliminarTemaController();
+	
+	
 }
