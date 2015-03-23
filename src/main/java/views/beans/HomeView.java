b4b03dcd4363cc9ba51f398c604ba57c20bbb53b
Jorge Lillo <jorge.lillo9@gmail.com>
2015-03-23 23:00:56 +0100

    ejemplo i18n

--- src/main/java/views/beans/HomeView.java
+++ src/main/java/views/beans/HomeView.java
@@ -2,10 +2,13 @@ package views.beans;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
+import java.util.ResourceBundle;
 
 import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ManagedProperty;
+import javax.faces.context.FacesContext;
 
 import models.entities.Tema;
 import controllers.ControllerFactory;
@@ -16,6 +19,26 @@ public class HomeView {
 	@ManagedProperty(value = "#{controllerFactory}")
 	private ControllerFactory controllerFactory;
 
+	 public HomeView() {
+	        this.locale = FacesContext.getCurrentInstance().getExternalContext().getRequestLocale();
+	   }
+	 
+	 public Locale getLocale() {
+	        return this.locale;
+	    }
+	 
+	 public String msg(String key) {
+	        return ResourceBundle.getBundle("i18n.messages", this.locale).getString(key);
+	    }
+	 
+	 public String change(String language) {
+	        this.locale = new Locale(language);
+	        FacesContext.getCurrentInstance().getViewRoot().setLocale(locale);
+	        return null;
+	    }
+	 
+	private Locale locale;
+	
 	private String password;
 
 	private String errorMsg;
